eb0f04dfa85977cdfc58dfc8057459a1ddb9fdb2
Pavel <malinatrashh@gmail.com>
2023-10-29 16:59:13 +0800

    6

--- Promblems/Medium/189. Rotate Array.js
+++ Promblems/Medium/189. Rotate Array.js
@@ -4,11 +4,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-	k %= nums.length;
-	nums.unshift(...nums.splice(nums.length - k, k));
-	console.log(nums);
-};
+	k %= nums.length
+	nums.unshift(...nums.splice(nums.length - k, k))
+	console.log(nums)
+}
 
-const nums = [1, 2, 3, 4, 5, 6, 7];
-const k = 3;
-rotate(nums, k);
+const nums = [1, 2, 3, 4, 5, 6, 7]
+const k = 3
+rotate(nums, k)
