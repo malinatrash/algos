87b43b9b58a53413eb13dff8e8dde54d14c611e0
Pavel <malinatrashh@gmail.com>
2023-11-04 21:55:21 +0800

    Update F.cpp

--- Promblems/Yandex/training4/Разминка/F.cpp
+++ Promblems/Yandex/training4/Разминка/F.cpp
@@ -2,34 +2,11 @@
 using namespace std;
 
 int main() {
-	int k,n;
+	int k,n; 
 	cin >> k >> n;
 
 	int a[n+1];
-	int remain = 0;
-	for (int i = 1; i < n +1; i++) {
-		cin >> a[i];
-		remain+=a[i];
-	}
-	int r = n;
-	int time = 0;
-	int i = 0;
-	while (remain != 0) {
-		time++;
-		i++;
-		if (a[i] >= n) {
-			r -= a[i] - a[i] % r;
-			a[i] = a[i] % r;
-			remain -= a[i] - a[i] % r;
-			time += i;
-			i = 0;
-			r = n;
-		} else {
-			r -= a[i];
-			remain -= a[i];
-			i++;
-		}
-	}
-	cout << time << endl;
+	for (int i = 1; i < n +1; i++) cin >> a[i];
+	
 	return 0;
 }
