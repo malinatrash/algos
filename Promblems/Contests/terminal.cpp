75f4b72166478f373aa8a95d18e420554881c651
Pavel Naumov <malinatrashh@gmail.com>
2024-01-21 17:04:52 +0800

    fix

--- Promblems/Contests/terminal.cpp
+++ Promblems/Contests/terminal.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 template< typename T >
 ostream& operator<<(ostream& out, vector<T> &V) {
-	for (T v: V) out << v << ' ';
-	return out;
+    for (T v : V) out << v << ' ';
+    return out;
 }
 
 void performL(int* x) {
@@ -15,53 +15,82 @@ void performL(int* x) {
     }
 }
 
-void performInput(int* x, int* y, vector<string>* text, char value) {
-     if (*y >= 0 && *y < text->size() && *x >= 0 && *x <= (*text)[*y].size()) {
-        (*text)[*y].insert((*text)[*y].begin() + *x, value);
+void performB(int* x) {
+    *x = 0;
+}
+
+void performE(int* x, int y, vector<string>* text) {
+	*x = (*text)[y].size();
+}
+
+void performN(int x, int* y, vector<string>* text) {
+    *y += 1;
+		text->push_back("");
+		for (int i = x; i < text[*y-1].size(); i++) {
+			text[*y].push_back(text[*y-1].at(x));
+		}
+}
+
+void performInput(int* x, int y, vector<string>* text, char value) {
+    if (y >= 0 && y < text->size() && *x >= 0 && *x <= (*text)[y].size()) {
+        (*text)[y].insert((*text)[y].begin() + *x, value);
+        *x += 1;
     }
 }
 
-void performR(int* x) {
+void performR(int* x, int y, vector<string>* text) {
+	if (*x < (*text)[y].size()) {
     *x += 1;
+	}
 }
 
 int main() {
-	int x = 0, y = 0;
-	vector<string> text;
+    int x = 0, y = 0;
+    vector<string> text;
+    text.push_back("");
 
-	int t;
-	cin >> t;
+    vector<vector<int>> st;
 
-	while (t--) {
-		string cmds;
-		cin >> cmds;
+    int t;
+    cin >> t;
 
-		for (auto i : cmds) {
-			switch (i) {
-				case 'L':
-					performL(&x);
-					break;
+    while (t--) {
+        string cmds;
+        cin >> cmds;
 
-				case 'R':
-					performR(&x);
-					break;
+        for (char i : cmds) {
+            switch (i) {
+                case 'L':
+                    performL(&x);
+                    break;
 
-				case 'N':
-					break;
+                case 'R':
+                    performR(&x, y, &text);
+                    break;
 
-				case 'B':
-					break;
+                case 'N':
+										performN(x, &y, &text);
+                    break;
 
-				case 'E':
-					break;
+                case 'B':
+										performB(&x);
+                    break;
 
-				default:
-					performInput(&x, &y, &text, i);
-					break;
-			}
-		}
-		cout << text;
+                case 'E':
+										performE(&x, y, &text);
+                    break;
+
+                default:
+                    performInput(&x, y, &text, i);
+                    break;
+            }
+            // cout << "\nCHAR IS: " << i << " x: " << x << " y: " << y << endl;
+        }
+    }
+	for (string i: text) {
+		cout << i;
+		cout << endl;
 	}
-
+	cout << text[1];
 	return 0;
 }
