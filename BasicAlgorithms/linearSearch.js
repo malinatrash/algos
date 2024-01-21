6080bafc6e9097271afa9f57eb080f17b364809c
Pavel <malinatrashh@gmail.com>
2024-01-21 19:35:07 +0800

    прибрался

--- BasicAlgorithms/linearSearch.js
+++
@@ -1,8 +0,0 @@
-const linearSearch = (array, item) => {
-	for (let i = 0; i < array.lenght; i++) {
-		if (array[i] == item) {
-			return i
-		}
-	}
-	return null
-}
--- Promblems/CodeForces/Перебор 2021.04.21/1.py
+++
@@ -1,6 +0,0 @@
-k, r = map(int, input().split())
-
-for x in range(1, 11):
-    if (x * k) % 10 == 0 or (x * k) % 10 == r:
-        print(x)
-        break
